437b9a0a6b1c00c96bf9df4537d67d45a76d60d4
Ľubomír <holmeister.lubomir@gmail.com>
2018-04-05 19:17:05 +0200

    Commit before install security-checker 	modified:   .idea/workspace.xml q q
    dified:   config/routes.yaml
    s
    
    q
    qs
    
    qwq
    wq

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,11 +10,36 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController
 {
+
+    /**
+     * @Route("/")
+     */
+
     public function homepage()
     {
         return new Response('OMG! My first page already! WOOO!');
     }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+
+    public function show($slug)
+    {
+        return new Response(sprintf('Future page to show the article %s', $slug));
+    }
+
+//    /**
+//     * @Route("/news/why-asteroids-taste-like-bacon")
+//     */
+//
+//    public function show()
+//    {
+//        return new Response('Future page to show one space article!');
+//    }
+
 }

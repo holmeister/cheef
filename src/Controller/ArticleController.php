cc7007b17efccd08a0a32c54ebdde9fa67e3c299
Ľubomír <holmeister.lubomir@gmail.com>
2018-04-08 20:25:05 +0200

    before try length

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,20 +9,22 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
 
-    /**
-     * @Route("/")
-     */
+     /**
+ * @Route("/")
+ */
 
     public function homepage()
     {
         return new Response('OMG! My first page already! WOOO!');
     }
+
 
     /**
      * @Route("/news/{slug}")
@@ -30,8 +32,33 @@ class ArticleController
 
     public function show($slug)
     {
-        return new Response(sprintf('Future page to show the article %s', $slug));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all - asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig' , [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
+
+}
+
+
+//
+// /**
+// * @Route("/")
+// */
+//
+//    public function homepage()
+//    {
+//        return new Response('OMG! My first page already! WOOO!');
+//    }
+
+
+
 
 //    /**
 //     * @Route("/news/why-asteroids-taste-like-bacon")
@@ -42,4 +69,13 @@ class ArticleController
 //        return new Response('Future page to show one space article!');
 //    }
 
-}
+
+
+///**
+// * @Route("/news/{slug}")
+// */
+//
+//public function show($slug)
+//{
+//    return new Response(sprintf('Future page to show the article %s', $slug));
+//}
